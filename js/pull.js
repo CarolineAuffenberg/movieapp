c92fe0d112843046487e7cc1a35c7e47bf9f9100
CarolineAuffenberg <carolineauffenberg@Carolines-MacBook-Pro.local>
2016-04-28 21:53:32 -0400

    Changes

--- js/pull.js
+++ js/pull.js
@@ -24,7 +24,6 @@
 
 
 
-
 // Our search function
 function runSearch(userText){
   var encodedText;
@@ -33,12 +32,22 @@ function runSearch(userText){
     $.each(data.Search, function(counter, movie) {
       console.log(movie);
       //document.write(movie.Title);
-      $('.results-area').append('<div class="movie-result">');
-      $('.results-area').append('<h3>' + movie.Title + '</h3>');
-      $('.results-area').append('<p>' + movie.Year + '</p>');
-      $('.results-area').append('<a href="http://www.imdb.com/title/' + movie.imdbID + '">' + 'IMDB</a>');
-      $('.results-area').append('<img src =' + movie.Poster + '>');
-      $('.results-area').append('</div>');
+
+      var theAppend = "";
+      // first, wrap the result in a div.  we can use this div to style each result
+      theAppend = theAppend + '<div class="movie-result">';
+      // set the movie title to have an H3, and include the year the movie was released
+      // you can style the title by using .movie-result h3; you can style the year by using .movie-result .year
+      theAppend = theAppend + '  <h3> ' + movie.Title + ' <span class="year">(' + movie.Year + ')</span></h3>';
+      // now add the image
+      theAppend = theAppend + '  <div class="image"><img src="'+ movie.Poster +'" class="movie-logo"></div>';
+      // now add the IMDB link
+      theAppend = theAppend + '  <p class=""><a href="http://www.imdb.com/title/'+ movie.imdbDB + '">Get more information on IMDB</a></p>';
+      // and close the .movie-result div
+      theAppend = theAppend + '</div>';
+
+      // now that we've built our output, we can append it!
+      $('.results-area').append(theAppend);
     });
   });
 }
